f17f41a3a260fc2db136ddac50120d1fb91faa8c
kaykaym01 <kaymoore01@gmail.com>
2022-09-19 18:21:25 -0400

    Finish Day 1 Build a JavaScript Drum Kit

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,21 +1,20 @@
-function playSound(e){
-        let key = e.code;
-    
-        const audio = document.querySelector(`audio[data-key="${key}"`);
-        if (!audio) return;
-        audio.currentTime = 0;
-        audio.play();
-        const letter = document.querySelector(`.key[data-key="${key}"`);
-        letter.classList.add('playing');
-    
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key=${e.code}]`);
+  const key = document.querySelector(`.key[data-key=${e.code}]`);
+  if (!audio) return; // stop function from running
+  audio.currentTime = 0; // rewind sound to start
+  audio.play();
+  key.classList.add("playing");
 }
 
 window.addEventListener("keydown", playSound);
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
-function removeTransition(e){
-    if (e.propertyName !== 'transform') return;
-    this.classList.remove('playing');
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
 }
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => {
+  key.addEventListener("transitionend", removeTransition);
+});
